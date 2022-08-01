ab813653a6d8406846bfd0b0eca5b067453742c6
wjacobs71086 <w.jacobs.632@gmail.com>
2022-07-31 18:02:29 -0700

    Completed chapter 6. On to Chapter 7

--- app/components/star-rating.js
+++ app/components/star-rating.js
@@ -7,7 +7,7 @@ export default class StarRatingComponent extends Component {
   get stars() {
     let stars = [];
     for (let i = 1; i <= this.maxRating; i++) {
-      stars.push({ rating: i, full: i < this.args.rating });
+      stars.push({ rating: i, full: i <= this.args.rating });
     }
     return stars;
   }

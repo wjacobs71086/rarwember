d05dcf50873e5e82bb49c862dcb8d304f426ba50
Wesley Jacobs <30844018+wjacobs71086@users.noreply.github.com>
2022-08-04 12:31:34 -0700

    Stopped on page 122

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -2,63 +2,27 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import Band from 'rarwe/models/band';
 import Song from 'rarwe/models/song';
+import fetch from 'fetch';
 
 export default class BandsRoute extends Route {
   @service catalog;
+  
+  async model() {
+    let response = await fetch('/bands');
+    let json = await response.json();
 
-  model() {
-    let blackDog = new Song({
-      title: 'Black Dog',
-      rating: 3,
-    });
+    for (let item of json.data) {
+      let { id, attributes, relationships } = item;
+      let rels = {};
 
-    let yellowLedbetter = new Song({
-      title: 'Yellow Ledbetter',
-      rating: 2,
-    });
+      for (let relationshipName in relationship) {
+        rels[relationshipName] = relationships[relationshipName].links.related;
+      }
 
-    let pretender = new Song({
-      title: 'The Pretender',
-      rating: 5,
-    });
-
-    let daughter = new Song({
-      title: 'Daughter',
-      rating: 2,
-    });
-
-    let ledZeppelin = new Band({
-      id: 'led-zeppelin',
-      name: 'Led Zeppelin',
-      songs: [blackDog],
-    });
-
-    let pearlJam = new Band({
-      id: 'pearl-jam',
-      name: 'Pearl Jam',
-      songs: [yellowLedbetter, daughter],
-    });
-
-    let fooFighters = new Band({
-      id: 'foo-fighters',
-      name: 'The Foo Fighters',
-      songs: [pretender],
-    });
-
-    this.catalog.add('band', ledZeppelin);
-    this.catalog.add('band', pearlJam);
-    this.catalog.add('band', fooFighters);
-
-    blackDog.band = ledZeppelin;
-    yellowLedbetter.band = pearlJam;
-    daughter.band = pearlJam;
-    pretender.band = fooFighters;
-
-    this.catalog.add('song', blackDog);
-    this.catalog.add('song', yellowLedbetter);
-    this.catalog.add('song', daughter);
-    this.catalog.add('song', pretender);
-
+      let record = new Band({ id, ...attributes }, rels);
+      this.catalog.add('band', record);
+    }
+    
     return this.catalog.bands;
   }
 }

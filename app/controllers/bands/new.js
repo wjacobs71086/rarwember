fe57824558213bfb526494624d35da728379a0c1
Wesley Jacobs <30844018+wjacobs71086@users.noreply.github.com>
2022-08-15 12:22:42 -0700

    paused on pg 182

--- app/controllers/bands/new.js
+++ app/controllers/bands/new.js
@@ -9,6 +9,25 @@ export default class BandsNewController extends Controller {
 
   @tracked name;
 
+  constructor() {
+    super(...arguments);
+    this.router.on('routeWillChange', (transition) => {
+      if (this.confirmedLeave) {
+        return;
+      }
+      if (transition.from.name === 'bands.new') {
+        if (this.name) {
+          let leave = window.confirm('You have unsaved changes. Are you sure?');
+          if (leave) {
+            this.confirmedLeave = true;
+          } else {
+            transition.abort();
+          }
+        }
+      }
+    });
+  }
+
   @action
   updateName(event) {
     this.name = event.target.value;
@@ -17,6 +36,6 @@ export default class BandsNewController extends Controller {
   @action
   async saveBand() {
     let band = await this.catalog.create('band', { name: this.name });
-    this.router.transitionTo('bands.band.songs',  band.id);
+    this.router.transitionTo('bands.band.songs', band.id);
   }
 }
--- app/routes/bands/band/index.js
+++ app/routes/bands/band/index.js
@@ -3,10 +3,9 @@ import { inject as service } from '@ember/service';
 
 export default class BandsBandIndexRoute extends Route {
   @service router;
-
   redirect(band) {
-    (band.description) ? 
-      this.router.transitionTo('bands.band.details') :
-      this.router.transitionTo('bands.band.songs')
+    band.description
+      ? this.router.transitionTo('bands.band.details')
+      : this.router.transitionTo('bands.band.songs');
   }
 }
